c996a004394956e30f7f4160a83e99e977478271
lewisxhe <lewishe@outlook.com>
2024-08-05 11:31:13 +0800

    Enable lv font 8 , 10 size

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -350,8 +350,8 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 
 /*Montserrat fonts with ASCII range and some symbols using bpp = 4
  *https://fonts.google.com/specimen/Montserrat*/
-#define LV_FONT_MONTSERRAT_8  0
-#define LV_FONT_MONTSERRAT_10 0
+#define LV_FONT_MONTSERRAT_8  1
+#define LV_FONT_MONTSERRAT_10 1
 #define LV_FONT_MONTSERRAT_12 1
 #define LV_FONT_MONTSERRAT_14 1
 #define LV_FONT_MONTSERRAT_16 1
